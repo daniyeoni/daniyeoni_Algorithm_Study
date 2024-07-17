99cd30e145a4b765e09382e152ed4724772eeb98
강다연 <134955384+daniyeoni@users.noreply.github.com>
2024-07-17 21:11:23 +0900

    [Bronze V] Title: 새싹, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ Python/백준/Bronze/25083. 새싹/새싹.py
@@ -0,0 +1,11 @@
+import sys
+
+def Finput():
+    sys.stdin.readline().rstrip()
+
+print("         ,r'\"7")
+print("r`-_   ,'  ,/")
+print(" \. \". L_r'")
+print("   `~\/")
+print("      |")
+print("      |")
