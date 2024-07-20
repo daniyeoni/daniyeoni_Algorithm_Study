3f2b043a2f6ec930dc1a0c72281e17ac4ff01ada
강다연 <134955384+daniyeoni@users.noreply.github.com>
2024-07-20 16:41:50 +0900

    [level 0] Title: 중앙값 구하기, Time: 0.06 ms, Memory: 33.5 MB -BaekjoonHub

--- JavaScript/프로그래머스/0/120811. 중앙값 구하기/중앙값 구하기.js
+++ JavaScript/프로그래머스/0/120811. 중앙값 구하기/중앙값 구하기.js
@@ -1,5 +1,8 @@
-function solution(array) {
-    array.sort(asc)
-    var answer = 0;
+function solution(array) {  
+    array.sort((a,b)=> a-b)
+    
+    var num = Math.floor((array.length)/2)
+    var answer = array[num]
     return answer;
+    
 }
