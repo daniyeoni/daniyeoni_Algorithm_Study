4a445f01cfe8484e4a3aa8f532fd0190957b6ef2
강다연 <134955384+daniyeoni@users.noreply.github.com>
2024-07-24 10:46:52 +0900

    [level 0] Title: 특정 문자 제거하기, Time: 0.18 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ JavaScript/프로그래머스/0/120826. 특정 문자 제거하기/특정 문자 제거하기.js
@@ -0,0 +1,16 @@
+function solution(my_string, letter) {
+    var answer = '';
+    var string = '';
+    
+    for (let i = 0; i < my_string.length; i++){
+        string = my_string.substring(i, i+1)
+        if(string != letter){
+            answer = answer + string
+            
+        }
+        else
+            string.replace('')
+            continue
+    }
+    return answer;
+}
